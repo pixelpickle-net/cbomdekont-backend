1518ac35c66a47a7de27140243351e8ffe37e5b7
mehmetsafabenli <mehmetsafabenli@gmail.com>
2024-10-01 00:38:53 +0300

    test

--- pkg/api/http/server.go
+++ pkg/api/http/server.go
@@ -137,7 +137,7 @@ func (s *Server) registerHandlers() {
 
 func (s *Server) registerMiddlewares() {
 	s.app.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://57.129.41.91:9091", "https://backend.pixelpickle.net", "https://pixelpickle.net"},
+		AllowOrigins:     []string{"http://57.129.41.91:9091", "https://backend.pixelpickle.net", "https://pixelpickle.net", "http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "HEAD", "PUT", "DELETE", "PATCH", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
